e9d3933419483f0a221e44a36010ee364c04a491
O Driscoll <robert.o.driscoll@sap.com>
2020-04-13 11:09:17 +0100

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geoCode = require('./utils/geoCode')
 const forecast = require('./utils/forcast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Deine paths for Exress config
 const publicDirPath = path.join(__dirname, '../public')
@@ -88,6 +89,6 @@ app.get('*' ,(req,res) => {
 
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000...')
+app.listen(port, () => {
+    console.log('server is up on port' + port)
 })
